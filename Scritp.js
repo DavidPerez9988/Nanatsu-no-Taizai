6aaab7b580eb9765d1c6830d8cf92c9c07e014cd
DavidPerez9988 <davidphuidobro@gmail.com>
2025-02-10 12:45:20 +0100

    Add files via upload

---
+++ Scritp.js
@@ -0,0 +1,17 @@
+/* script.js */
+document.addEventListener('DOMContentLoaded', function() {
+    const leerMasBtns = document.querySelectorAll('.leer-mas-btn');
+
+    leerMasBtns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            const contenidoAdicional = this.previousElementSibling;
+            if (contenidoAdicional.style.display === 'none') {
+                contenidoAdicional.style.display = 'block';
+                this.textContent = 'Leer menos';
+            } else {
+                contenidoAdicional.style.display = 'none';
+                this.textContent = 'Leer más';
+            }
+        });
+    });
+});
